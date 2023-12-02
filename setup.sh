22af956ad8cc6353f905fcf453a5aa520747f9c7
ningyu1991 <ningyu.hust@gmail.com>
2023-12-02 14:05:45 -0500

    Update README.md

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@
 
 ln -fs /usr/share/zoneinfo/America/Los_Angelos /etc/localtime
 cp executables/chromedriver /usr/bin/chromedriver
+apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get --assume-yes install ./executables/google-chrome-stable_current_amd64.deb
 pip install -r requirements.txt
